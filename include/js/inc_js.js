7d30da776bfa42ad7a0a5c8d47b71b4b23a36b41
joshua <makulajosh@gmail.com>
2020-06-07 01:07:45 +0300

    mission bootcamp added to github

--- include/js/inc_js.js
+++ include/js/inc_js.js
@@ -1,4 +1,4 @@
-<script>
+
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
@@ -26,6 +26,6 @@ function includeHTML() {
     }
   }
 }
-</script>
+
 
 includeHTML();
